a267415ac0c63d8c3135966e30f818f237156f98
atlfsj <3321713512@qq.com>
2024-01-22 23:47:27 +0800

    添加theme属性和基础样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,6 +1,9 @@
 <template>
     <div>
         <Button>你好</Button>
+        <Button theme="button">你好</Button>
+        <Button theme="link">你好</Button>
+        <Button theme="text">你好</Button>
     </div>
 </template>
 
@@ -8,5 +11,11 @@
 import Button from '../lib/Button.vue'
 export default {
     components: { Button },
+    setup(){
+        const onClick = ()=> {
+            console.log('hi')
+        }
+        return {onClick}
+    }
 }
 </script>

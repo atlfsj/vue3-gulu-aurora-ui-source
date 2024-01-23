6cfd2edaf92040ed5e9a53880a4e50916c4a1849
atlfsj <3321713512@qq.com>
2024-01-23 16:47:01 +0800

    css最小影响原则

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -19,7 +19,7 @@ export default {
 <style lang="scss">
 $h: 22px;
 $h2: $h - 4px;
-gulu-switch {
+.gulu-switch {
   height: $h; width: $h * 2; border: none; background: #bfbfbf; border-radius: $h/2; position: relative;
   > span {
     position: absolute; top: 2px; left: 2px; height: $h2; width: $h2; background: white; border-radius: $h2 / 2; transition: all 250ms;

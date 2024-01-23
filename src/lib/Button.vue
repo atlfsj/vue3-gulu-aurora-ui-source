5779e92e015394851e942718c124ad2d0e78b10b
atlfsj <3321713512@qq.com>
2024-01-23 17:02:42 +0800

    支持theme属性

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -22,32 +22,49 @@ $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
 .gulu-button {
-box-sizing: border-box;
-height: $h;
-padding: 0 12px;
-cursor: pointer;
-display: inline-flex;
-justify-content: center;
-align-items: center;
-white-space: nowrap;
-background: white;
-color: $color;
-border: 1px solid $border-color;
-border-radius: $radius;
-box-shadow: 0 1px 0 fade-out(black, 0.95);
-& + & {
+  box-sizing: border-box;
+  height: $h;
+  padding: 0 12px;
+  cursor: pointer;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  white-space: nowrap;
+  background: white;
+  color: $color;
+  border: 1px solid $border-color;
+  border-radius: $radius;
+  box-shadow: 0 1px 0 fade-out(black, 0.95);
+  & + & {
     margin-left: 8px;
-}
-&:hover,
-&:focus {
+  }
+  &:hover,
+  &:focus {
     color: $blue;
     border-color: $blue;
-}
-&:focus {
+  }
+  &:focus {
     outline: none;
-}
-&::-moz-focus-inner {
+  }
+  &::-moz-focus-inner {
     border: 0;
-}
+  }
+  &.gulu-theme-link{
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,&:focus{
+      color: lighten($blue, 10%);
+    }
+  }
+  &.gulu-theme-text{
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,&:focus{
+      background: darken(white, 5%);;
+    }
+
+  }
 }
 </style>

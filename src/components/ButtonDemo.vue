3065ecf0626e39aaf3b2a5f33a158fd3483791ed
atlfsj <3321713512@qq.com>
2024-01-24 11:44:49 +0800

    支持level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -8,20 +8,41 @@
     </div>
     <h1>示例2</h1>
     <div>
+      <div>
       <Button size="big">大大大</Button>
       <Button>普普通</Button>
       <Button size="small">小小小</Button>
-    </div>
+      </div>
+      <div>
+        <Button theme="link" size="big">大大大</Button>
+        <Button theme="link">普普通</Button>
+        <Button theme="link" size="small" >小小小</Button>
+      </div>
+      <div>
+        <Button theme="text" size="big" >大大大</Button>
+        <Button theme="text">普普通</Button>
+        <Button theme="text" size="small">小小小</Button>
+      </div>
+    </div>  
+    <h1>示例3</h1>
     <div>
-      <Button theme="link" size="big">大大大</Button>
-      <Button theme="link">普普通</Button>
-      <Button size="small" theme="link">小小小</Button>
+      <div>
+        <Button level="main">主要按钮</Button>
+        <Button>普通按钮</Button>
+        <Button level="danger">危险按钮</Button>
+      </div>
+      <div>
+        <Button theme="link" level="main">主要按钮</Button>
+        <Button theme="link">普通按钮</Button>
+        <Button theme="link" level="danger">危险按钮</Button>
+      </div>
+      <div>
+        <Button theme="text" level="main">主要按钮</Button>
+        <Button theme="text">普通按钮</Button>
+        <Button theme="text" level="danger">危险按钮</Button>
+      </div>
     </div>
-    <div>
-      <Button size="big" theme="text">大大大</Button>
-      <Button theme="text">普普通</Button>
-      <Button size="small" theme="text">小小小</Button>
-    </div>
+    
 </template>
 
 <script lang="ts">

b662bd7148df086a8dc8799a4c1afe19b290582b
atlfsj <3321713512@qq.com>
2024-01-24 12:13:51 +0800

    支持加载中

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -50,7 +50,10 @@
         <Button theme="text" disabled>禁用文字按钮</Button>
       </div>
     </div>
-    
+    <h1>示例5</h1>
+    <div>
+      <Button loading>加载中</Button>
+    </div>
 </template>
 
 <script lang="ts">
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,42 +1,46 @@
 <template>
-    <button class="gulu-button" :class="classes" :disabled="disabled">
-      <slot />
-    </button>
+  <button class="gulu-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="gulu-loadingIndicator"></span>
+    <slot />
+  </button>
 </template>
 
 <script lang="ts">
-import { computed } from 'vue';
-
+import { computed } from "vue";
 export default {
-    props: {
-        theme: {
-            type: String,
-            default: "button",
-        },
-        size: {
-            type: String,
-            default: "normal",
-        },
-        level: {
-          type: String,
-          default: "normal",
-        },
-        disabled: {
-          type: Boolean,
-          default: "false",
-        }
+  props: {
+    theme: {
+      type: String,
+      default: "button",
     },
-    setup(props){
-        const { theme, size, level } = props;
-        const classes = computed(() => {
-            return {
-                [`gulu-theme-${theme}`]: theme,
-                [`gulu-size-${size}`]: size,
-                [`gulu-level-${level}`]: level,
-            }
-        })
-        return { classes }
+    size: {
+      type: String,
+      default: "normal",
+    },
+    level: {
+      type: String,
+      default: "normal",
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    loading: {
+      type: Boolean,
+      default: false
     }
+  },
+  setup(props) {
+    const { theme, size, level } = props;
+    const classes = computed(() => {
+      return {
+        [`gulu-theme-${theme}`]: theme,
+        [`gulu-size-${size}`]: size,
+        [`gulu-level-${level}`]: level,
+      };
+    });
+    return { classes };
+  },
 };
 </script>
 
@@ -167,5 +171,20 @@ $grey: grey;
       color: $grey;
     }
   }
+  > .gulu-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px; 
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: gulu-spin 1s infinite linear;
+  }
+}
+@keyframes gulu-spin {
+  0%{transform: rotate(0deg)} 
+  100%{transform: rotate(360deg)} 
 }
 </style>

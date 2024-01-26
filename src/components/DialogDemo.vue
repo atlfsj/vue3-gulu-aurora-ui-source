dc984d48b05bab07f4afdca3dbb0d48fcf180226
atlfsj <3321713512@qq.com>
2024-01-26 11:11:17 +0800

    dialog支持点击关闭

--- src/components/DialogDemo.vue
+++ src/components/DialogDemo.vue
@@ -2,15 +2,13 @@
 <div>Dialog 示例</div>
 <h1>示例1</h1>
 <Button @click="toggle">toggle</Button>
-<Dialog v-model:visible="x"></Dialog>
+<Dialog v-model:visible="x" :closeOnClickOverlay="true" :ok="f1" :cancel="f2"></Dialog>
 </template>
 
 <script lang="ts">
 import Dialog from '../lib/Dialog.vue'
 import Button from '../lib/Button.vue'
-import {
-    ref
-} from 'vue'
+import { ref } from 'vue'
 export default {
     components: {
         Dialog,
@@ -21,9 +19,16 @@ export default {
         const toggle = () => {
             x.value = !x.value
         }
+        const f1 = ()=> {
+            console.log('ok')
+        }
+        const f2 = ()=> {
+            console.log('cancel')
+        }
         return {
             x,
-            toggle
+            toggle,
+            f1,f2
         }
     }
 }

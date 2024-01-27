b18668069019ae95904ac7ca11af5bcd3ad661f3
atlfsj <3321713512@qq.com>
2024-01-27 21:57:54 +0800

    使用css切换内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,66 +1,88 @@
 <template>
     <div class="gulu-tabs">
-        <div class="gulu-tabs-nav">
-            <div class="gulu-tabs-nav-item"  :class="{selected: t===selected}"
-            v-for="(t,index) in titles" :key="index">{{t}}</div>
-        </div>
-        <div class="gulu-tabs-content">
-            <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
-        </div>
+      <div class="gulu-tabs-nav">
+        <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+      </div>
+      <div class="gulu-tabs-content">
+        <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
+      </div>
     </div>
 </template>
-
+    
 <script lang="ts">
 import Tab from './Tab.vue'
-export default { 
+import { computed } from 'vue'
+export default {
     props: {
         selected: {
             type: String
         }
     },
-    components: {
-        Tab
-    },
     setup(props, context) {
         const defaults = context.slots.default()
-        defaults.forEach((tag)=> {
-            if(tag.type !== Tab){
+        defaults.forEach((tag) => {
+            if (tag.type !== Tab) {
                 throw new Error('Tabs 子标签必须是 Tab')
             }
+        })
+        const current = computed(() => {
+            console.log('重新 return')
+            return defaults.filter((tag) => {
+                return tag.props.title === props.selected
+            })[0]
         })
         const titles = defaults.map((tag) => {
             return tag.props.title
         })
+        const select = (title: string) => {
+            context.emit('update:selected', title)
+        }
         return {
-            defaults, titles
+            defaults,
+            titles,
+            current,
+            select
         }
     }
 }
 </script>
-
+    
 <style lang="scss">
 $blue: #40a9ff;
 $color: #333;
 $border-color: #d9d9d9;
+
 .gulu-tabs {
-  &-nav {
+    &-nav {
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+
     &-item {
-      padding: 8px 0;
-      margin: 0 16px;
-      cursor: pointer;
-      &:first-child {
+        padding: 8px 0;
+        margin: 0 16px;
+        cursor: pointer;
+
+        &:first-child {
         margin-left: 0;
-      }
-      &.selected {
+        }
+
+        &.selected {
         color: $blue;
-      }
+        }
     }
-  }
-  &-content {
+    }
+
+    &-content {
     padding: 8px 0;
-  }
+
+    &-item {
+        display: none;
+
+        &.selected {
+        display: block;
+        }
+    }
+    }
 }
 </style>

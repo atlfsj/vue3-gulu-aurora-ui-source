95fe1db8dc2ee6a50d9b16f31c35f54657f72c88
atlfsj <3321713512@qq.com>
2024-01-27 18:08:05 +0800

    导航1被选中

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,14 +1,23 @@
 <template>
-    <div>
-        <div v-for="(t,index) in titles" :key="index">{{ t }}</div>    
-        <!-- <component :is="xxx">表示动态切换要渲染的组件 -->
-        <component v-for="(c,index) in defaults" :is="c" :key="index" />
+    <div class="gulu-tabs">
+        <div class="gulu-tabs-nav">
+            <div class="gulu-tabs-nav-item"  :class="{selected: t===selected}"
+            v-for="(t,index) in titles" :key="index">{{t}}</div>
+        </div>
+        <div class="gulu-tabs-content">
+            <component class="gulu-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+        </div>
     </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue'
 export default { 
+    props: {
+        selected: {
+            type: String
+        }
+    },
     components: {
         Tab
     },
@@ -28,3 +37,30 @@ export default {
     }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.gulu-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>

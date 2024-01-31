76dd0ca3b2817a8f7414db211932af9adc5a85ca
atlfsj <3321713512@qq.com>
2024-01-31 18:54:27 +0800

    给title加下划线

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -2,51 +2,55 @@
     <div class="gulu-tabs">
       <div class="gulu-tabs-nav">
         <div class="gulu-tabs-nav-item" v-for="(t,index) in titles" @click="select(t)" :class="{selected: t=== selected}" :key="index">{{t}}</div>
+        <div class="gulu-tabs-nav-indicator"></div>
       </div>
       <div class="gulu-tabs-content">
         <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
+    
       </div>
     </div>
 </template>
-    
+
 <script lang="ts">
 import Tab from './Tab.vue'
-import { computed } from 'vue'
+import {
+    computed
+} from 'vue'
 export default {
     props: {
-        selected: {
-            type: String
-        }
+    selected: {
+        type: String
+    }
     },
     setup(props, context) {
-        const defaults = context.slots.default()
-        defaults.forEach((tag) => {
-            if (tag.type !== Tab) {
-                throw new Error('Tabs 子标签必须是 Tab')
-            }
-        })
-        const current = computed(() => {
-            console.log('重新 return')
-            return defaults.filter((tag) => {
-                return tag.props.title === props.selected
-            })[0]
-        })
-        const titles = defaults.map((tag) => {
-            return tag.props.title
-        })
-        const select = (title: string) => {
-            context.emit('update:selected', title)
+    const defaults = context.slots.default()
+    defaults.forEach((tag) => {
+        if (tag.type !== Tab) {
+        throw new Error('Tabs 子标签必须是 Tab')
         }
-        return {
-            defaults,
-            titles,
-            current,
-            select
-        }
+    })
+    const current = computed(() => {
+        console.log('重新 return')
+        return defaults.filter((tag) => {
+        return tag.props.title === props.selected
+        })[0]
+    })
+    const titles = defaults.map((tag) => {
+        return tag.props.title
+    })
+    const select = (title: string) => {
+        context.emit('update:selected', title)
+    }
+    return {
+        defaults,
+        titles,
+        current,
+        select
+    }
     }
 }
 </script>
-    
+
 <style lang="scss">
 $blue: #40a9ff;
 $color: #333;
@@ -57,6 +61,7 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+    position: relative;
 
     &-item {
         padding: 8px 0;
@@ -70,6 +75,15 @@ $border-color: #d9d9d9;
         &.selected {
         color: $blue;
         }
+    }
+
+    &-indicator {
+        position: absolute;
+        height: 3px;
+        background: $blue;
+        left: 0;
+        bottom: -1px;
+        width: 100px;
     }
     }
 

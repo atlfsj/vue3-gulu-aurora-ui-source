f0b18885e885a24f50deaecd07785f66275c6935
atlfsj <3321713512@qq.com>
2024-01-31 20:13:34 +0800

    :is="current" :key实现Tab切换

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -5,8 +5,7 @@
         <div class="gulu-tabs-nav-indicator" ref="indicator"></div>
       </div>
       <div class="gulu-tabs-content">
-        <component class="gulu-tabs-content-item" :class="{selected: c.props.title === selected }" v-for="c in defaults" :is="c" />
-    
+        <component :is="current" :key="current.props.title" />
       </div>
     </div>
 </template>
@@ -21,51 +20,55 @@ import {
 } from 'vue'
 export default {
     props: {
-    selected: {
-        type: String
-    }
+        selected: {
+            type: String
+        }
     },
     setup(props, context) {
-    const selectedItem = ref < HTMLDivElement > (null)
-    const indicator = ref < HTMLDivElement > (null)
-    const container = ref < HTMLDivElement > (null)
-    onMounted(() => {
-        watchEffect(() => {
-        const {
-            width
-        } = selectedItem.value.getBoundingClientRect()
-        indicator.value.style.width = width + 'px'
-        const {
-            left: left1
-        } = container.value.getBoundingClientRect()
-        const {
-            left: left2
-        } = selectedItem.value.getBoundingClientRect()
-        const left = left2 - left1
-        indicator.value.style.left = left + 'px'
+        const selectedItem = ref < HTMLDivElement > (null)
+        const indicator = ref < HTMLDivElement > (null)
+        const container = ref < HTMLDivElement > (null)
+        onMounted(() => {
+            watchEffect(() => {
+            const {
+                width
+            } = selectedItem.value.getBoundingClientRect()
+            indicator.value.style.width = width + 'px'
+            const {
+                left: left1
+            } = container.value.getBoundingClientRect()
+            const {
+                left: left2
+            } = selectedItem.value.getBoundingClientRect()
+            const left = left2 - left1
+            indicator.value.style.left = left + 'px'
+            })
         })
-    })
 
-    const defaults = context.slots.default()
-    defaults.forEach((tag) => {
-        if (tag.type !== Tab) {
-        throw new Error('Tabs 子标签必须是 Tab')
+        const defaults = context.slots.default()
+        defaults.forEach((tag) => {
+            if (tag.type !== Tab) {
+            throw new Error('Tabs 子标签必须是 Tab')
+            }
+        })
+        const current = computed(() => {
+        return defaults.find(tag => tag.props.title === props.selected)
+        })
+        const titles = defaults.map((tag) => {
+            return tag.props.title
+        })
+        const select = (title: string) => {
+            context.emit('update:selected', title)
         }
-    })
-    const titles = defaults.map((tag) => {
-        return tag.props.title
-    })
-    const select = (title: string) => {
-        context.emit('update:selected', title)
-    }
-    return {
-        defaults,
-        titles,
-        select,
-        selectedItem,
-        indicator,
-        container
-    }
+        return {
+            current,
+            defaults,
+            titles,
+            select,
+            selectedItem,
+            indicator,
+            container
+        }
     }
 }
 </script>
@@ -77,46 +80,38 @@ $border-color: #d9d9d9;
 
 .gulu-tabs {
     &-nav {
-    display: flex;
-    color: $color;
-    border-bottom: 1px solid $border-color;
-    position: relative;
+        display: flex;
+        color: $color;
+        border-bottom: 1px solid $border-color;
+        position: relative;
 
-    &-item {
-        padding: 8px 0;
-        margin: 0 16px;
-        cursor: pointer;
+        &-item {
+            padding: 8px 0;
+            margin: 0 16px;
+            cursor: pointer;
 
-        &:first-child {
-        margin-left: 0;
+            &:first-child {
+            margin-left: 0;
+            }
+
+            &.selected {
+            color: $blue;
+            }
         }
 
-        &.selected {
-        color: $blue;
+        &-indicator {
+            position: absolute;
+            height: 3px;
+            background: $blue;
+            left: 0;
+            bottom: -1px;
+            width: 100px;
+            transition: all 250ms;
         }
-    }
-
-    &-indicator {
-        position: absolute;
-        height: 3px;
-        background: $blue;
-        left: 0;
-        bottom: -1px;
-        width: 100px;
-        transition: all 250ms;
-    }
     }
 
     &-content {
-    padding: 8px 0;
-
-    &-item {
-        display: none;
-
-        &.selected {
-        display: block;
-        }
-    }
+        padding: 8px 0;   
     }
 }
 </style>

78a26d40851fca2040ca03cc1190134848d91af7
atlfsj <3321713512@qq.com>
2024-02-01 18:35:56 +0800

    features 样式

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -6,9 +6,10 @@
           </svg>
         </div>
         <ul class="menu">
-            <li>菜单1</li>
-            <li>菜单2</li>
-        </ul>
+          <li>
+            <router-link to="/doc">文档</router-link>
+          </li>
+        </ul>  
         <span class="toggleAside"  @click="toggleAside"></span>
     </div>
 </template>

948db4b98d865df07cbaca3b9c1023617d08adb1
atlfsj <3321713512@qq.com>
2024-02-03 23:32:50 +0800

    事不过三：封装Markdown

--- src/router.ts
+++ src/router.ts
@@ -1,27 +1,28 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import Home from './views/Home.vue'
-import Doc from './views/Doc.vue'
-import SwitchDemo from './components/SwitchDemo.vue'
-import ButtonDemo from './components/ButtonDemo.vue'
-import DialogDemo from './components/DialogDemo.vue'
-import TabsDemo from './components/TabsDemo.vue'
-import DocDemo from './components/DocDemo.vue'
-import Intro from './views/Intro.vue'
-import GetStarted from './views/GetStarted.vue'
-import Install from './views/Install.vue'
-const history = createWebHashHistory()
+import { createWebHashHistory, createRouter } from "vue-router";
+import Home from "./views/Home.vue";
+import Doc from "./views/Doc.vue";
+import SwitchDemo from "./components/SwitchDemo.vue";
+import ButtonDemo from "./components/ButtonDemo.vue";
+import DialogDemo from "./components/DialogDemo.vue";
+import TabsDemo from "./components/TabsDemo.vue";
+import DocDemo from "./components/DocDemo.vue";
 
+import { h } from 'vue';
+import Markdown from './components/Markdown.vue';
+const history = createWebHashHistory();
+const md = filename => h(Markdown, { path: `../markdown/${filename}.md`, key: filename })
 export const router = createRouter({
     history: history,
     routes: [
         { path: "/", component: Home },
         {
-            path: "/doc", component: Doc,
+            path: "/doc",
+            component: Doc,
             children: [
                 { path: "", component: DocDemo },
-                { path: "intro", component: Intro },
-                { path: "get-started", component: GetStarted },
-                { path: "install", component: Install },
+                { path: "intro", component: md('intro') },
+                { path: "get-started", component: md('getStarted') },
+                { path: "install", component: md('install') },
                 { path: "switch", component: SwitchDemo },
                 { path: "button", component: ButtonDemo },
                 { path: "dialog", component: DialogDemo },
@@ -30,4 +31,6 @@ export const router = createRouter({
         },
     ],
 });
-
+router.afterEach(() => {
+    console.log("路由切换了");
+});
